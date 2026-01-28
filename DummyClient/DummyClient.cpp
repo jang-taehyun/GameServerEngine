96086a3b1efd4bb4f0d73d62bb72f3732f047d41
jang-taehyun <jth0202@naver.com>
2026-01-28 17:55:18 +0900

    4-39) socket programming 기초

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -1,8 +1,79 @@
 #include "pch.h"
 #include <iostream>
+#include <chrono>
+
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
 
 int main()
 {
+    int32 errorCode = 0;
+    WSADATA wsaData;
+
+    // 윈속 초기화 (ws2_32 라이브러리 초기화) //
+    // 관련 정보가 wsaData에 채워진다.
+    if (errorCode = ::WSAStartup(MAKEWORD(2, 2), &wsaData))
+    {
+        return errorCode;
+    }
+
+    // socket 생성 //
+    // af : address family (AF_INET : IPv4, AF_INET6 : IPv6)
+    // type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+    // protocol : 0
+    // return : descriptor
+    SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (INVALID_SOCKET == clientSocket)
+    {
+        errorCode = ::WSAGetLastError();
+        std::cout << "Socket ErrorCode : " << errorCode << std::endl;
+        return 0;
+    }
+
+    // 서버 정보 입력 //
+    // Little-Endian vs Big-Endian
+    // Little-Endian : low [0x78][0x56][0x34][0x12] high
+    // -> Little-Endian은 주로 Intel CPU에서 사용
+    // Big-Endian    : low [0x12][0x34][0x56][0x78] high
+    // -> network 표준은 Big-Endian
+    SOCKADDR_IN serverAddr;
+    ::memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    ::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);    // ::inet_addr() 함수는 deprecated이기 때문에, 최근에는 ::inet_pton() 함수를 사용한다.
+    serverAddr.sin_port = htons(7777);                         // -> host to network short
+
+    // server에 연결 요청 //
+    // TCP에서는 3-way handshaking이 실행된다.
+    if (SOCKET_ERROR == ::connect(clientSocket, reinterpret_cast<SOCKADDR*>(&serverAddr), sizeof(serverAddr)))
+    {
+        errorCode = ::WSAGetLastError();
+        std::cout << "connect() ErrorCode : " << errorCode << std::endl;
+        return 0;
+    }
+
+    // -----------------------------------------------
+    // 연결 성공! 이제부터 데이터 송수신 가능!
+    // -----------------------------------------------
+
+    std::cout << "Connected To Server!" << std::endl;
+    
+    while (true)
+    {
+        // TODO
+
+        {
+            using namespace std::chrono;
+            std::this_thread::sleep_for(1s);
+        }
+    }
+
+    // socket 리소스 반환 //
+    ::closesocket(clientSocket);
+
+    // 윈속 종료 //
+    ::WSACleanup();
 
     return 0;
 }
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -1,158 +1,113 @@
 #include "pch.h"
 #include <iostream>
-#include "CorePch.h"
+
 #include <atomic>
 #include <mutex>
-#include <windows.h>
 #include <future>
 #include <thread>
 #include <chrono>
 
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+
 #include "ThreadManager.h"
-#include "Memory.h"
-
-/**
-* MS에서 제공하는 SLIST_ENTRY
-* -> MS에서 제공하는 Lock-free List
-* -> Lock-free stack을 만들때 사용된다.
-* 
-* MS에서 제공하는 SLIST_ENTRY를 사용하는 2가지 방법
-* 1) SLIST_ENTRY를 상속
-*   -> 포인터의 casting이 가능해짐
-*   -> 상속을 받는다면, SLIST_ENTRY 안에 있는 멤버 변수들을 맨처음으로 받을 수 있음
-* 2) SLIST_ENTRY를 멤버 변수의 맨처음 변수로 갖는다.
-* 
-* SLIST_ENTRY를 사용한다면,
-* -> header를 만들고 초기화해줘야 한다.
-*   -> PSLIST_HEADER Gheader;
-* -> 반드시 데이터(객체)는 16byte로 정렬해야 한다.
-*   -> DECLSPEC_ALIGN(16)
-*/
-
-using TL = TypeList<
-    class Player,
-    class Knight,
-    class Mage,
-    class Archer
->;
-
-class Player
-{
-public:
-    Player()
-    {
-        INIT_TL(Player);
-    }
-
-    DECLARE_TL;
-};
-
-class Knight : public Player
-{
-public:
-    Knight()
-    {
-        INIT_TL(Knight);
-    }
-};
-
-class Mage : public Player
-{
-public:
-    Mage()
-    {
-        INIT_TL(Mage);
-    }
-};
-
-class Archer : public Player
-{
-public:
-    Archer()
-    {
-        INIT_TL(Archer);
-    }
-};
-
-class Dog
-{
-
-};
 
 int main()
 {
-    TypeList<Player, Knight, Mage>::Head whoAmI1;
-    TypeList<Player, Knight, Mage>::Tail::Head whoAmI2;
-    TypeList<Player, Knight, Mage>::Tail::Tail whoAmI3;
+    int32 errorCode = 0;
+    WSADATA wsaData;
 
-    TypeList<Player, TypeList<Knight, Mage>>::Head whoAmI4;
-    TypeList<Player, TypeList<Knight, Mage>>::Tail::Tail whoAmI10;
-
-    int32 n1 = Length<TL>::value;
-    int32 n2 = Length<TypeList<Player, TypeList<Knight, Mage>>>::value;
-
-    TypeAt<TL, 0>::Result whoAmI5;
-    TypeAt<TL, 1>::Result whoAmI6;
-    TypeAt<TL, 2>::Result whoAmI7;
-    TypeAt<TL, 3>::Result whoAmI8;
-
-    int32 idx1 = IndexOf<TL, Player>::value;
-    int32 idx2 = IndexOf<TL, Knight>::value;
-    int32 idx3 = IndexOf<TL, Archer>::value;
-    int32 idx4 = IndexOf<TL, Mage>::value;
-    int32 idx5 = IndexOf<TL, Dog>::value;
-
-    bool IsCanCast1 = Conversion<Knight, Player>::exist;
-    bool IsCanCast2 = Conversion<Player, Knight>::exist;
-    bool IsCanCast3 = Conversion<Knight, Dog>::exist;
-    bool IsCanCast4 = TypeConversion<TL>::CanConvert(0, 1);
-
+    // 윈속 초기화 (ws2_32 라이브러리 초기화) //
+    // 관련 정보가 wsaData에 채워진다.
+    if (errorCode = ::WSAStartup(MAKEWORD(2, 2), &wsaData))
     {
-        Player* player = new Knight;
+        return errorCode;
+    }
 
-        bool canCast = CanCast<Knight*>(player);
-        Knight* knight = TypeCast<Knight*>(player);
-        if (knight)
+    // socket 생성 //
+    // af : address family (AF_INET : IPv4, AF_INET6 : IPv6)
+    // type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+    // protocol : 0
+    // return : descriptor
+    SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (INVALID_SOCKET == listenSocket)
+    {
+        errorCode = ::WSAGetLastError();
+        std::cout << "Socket ErrorCode : " << errorCode << std::endl;
+        return 0;
+    }
+
+    // 내 정보(서버의 정보) 입력 //
+    // Little-Endian vs Big-Endian
+    // Little-Endian : low [0x78][0x56][0x34][0x12] high
+    // -> Little-Endian은 주로 Intel CPU에서 사용
+    // Big-Endian    : low [0x12][0x34][0x56][0x78] high
+    // -> network 표준은 Big-Endian
+    SOCKADDR_IN serverAddr;
+    ::memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);           // -> 니가 알아서 IP를 골라줘
+    serverAddr.sin_port = htons(7777);                          // -> host to network short
+
+    // 내 정보를 socket에 바인딩 //
+    if (SOCKET_ERROR == ::bind(listenSocket, reinterpret_cast<SOCKADDR*>(&serverAddr), sizeof(serverAddr)))
+    {
+        errorCode = ::WSAGetLastError();
+        std::cout << "bind() ErrorCode : " << errorCode << std::endl;
+        return 0;
+    }
+
+    // listen socket을 연결 요청 대기 상태로 전환 //
+    // back log queue : 대기열의 최대값
+    // -> 연결 요청이 들어오면(외부에서 listen() 함수가 호출되면),
+    //    back log queue에 삽입되고,
+    // -> accept() 함수가 호출될 때마다 back log queue에서 하나씩 꺼내온다.
+    // TCP에서 back log queue에 들어왔다는 뜻은 3-way handshaking이 성공했다는 뜻
+    if (SOCKET_ERROR == ::listen(listenSocket, 10))
+    {
+        errorCode = ::WSAGetLastError();
+        std::cout << "listen() ErrorCode : " << errorCode << std::endl;
+        return 0;
+    }
+
+    // -----------------------------------------------
+    
+    while (true)
+    {
+        SOCKADDR_IN clientAddr;
+        ::memset(&clientAddr, 0, sizeof(clientAddr));
+
+        int32 clientAddrSize = sizeof(clientAddr);
+
+        // client의 연결 요청을 받아들인다. //
+        // -> back log queue에서 하나 꺼내온다.
+        SOCKET clientSocket = ::accept(listenSocket, reinterpret_cast<SOCKADDR*>(&clientAddr), &clientAddrSize);
+        if (INVALID_SOCKET == clientSocket)
         {
-
+            errorCode = ::WSAGetLastError();
+            std::cout << "accept() ErrorCode : " << errorCode << std::endl;
+            return 0;
         }
 
-        delete player;
+        // 손님 입장!
+        char ipAddress[16] = { 0, };
+        ::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+        std::cout << "Client Conntected! IP : " << ipAddress << std::endl;
+
+        // TODO
+        // socket을 통해 전송을 희망하는 데이터는,
+        // 커널 영역에 있는 buffer에 복사된 후 전송된다.
     }
 
-    {
-        Player* player = new Player;
+    // -----------------------------------------------
 
-        bool canCast = CanCast<Knight*>(player);
-        Knight* knight = TypeCast<Knight*>(player);
+    // socket 리소스 반환 //
+    ::closesocket(listenSocket);
 
-        delete player;
-    }
-
-    {
-        std::shared_ptr<Knight> knight = std::make_shared<Knight>();
-
-        bool canCast = CanCast<Player>(knight);
-        std::shared_ptr<Player> player = TypeCast<Player>(knight);
-
-        int32 BreakPoint = 1;
-    }
-
-    for (int32 i = 0; i < 5; ++i)
-    {
-        GThreadManager->Launch(
-            []()
-            {
-                using namespace std::chrono;
-
-                while (true)
-                {
-                }
-            }
-        );
-    }
-
-    GThreadManager->Join();
+    // 윈속 종료 //
+    ::WSACleanup();
 
     return 0;
 }
