1324e4a273dd6cee43a9a00219ce37f699f14848
jang-taehyun <jth0202@naver.com>
2026-01-29 19:43:44 +0900

    4-43) setsockopt() 함수의 매개 변수 정리, Nagle 알고리즘, half-close

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -23,63 +23,94 @@ int main()
         return errorCode;
     }
 
-    SOCKET clientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+    /**
+    * socket이 생성될 때, kernel 영역에서 send buffer, recv buffer가 만들어진다.
+    * -> send buffer, recv buffer의 크기 변경 가능
+    * 
+    * socket의 옵션을 설정하는 함수
+    * -> ::setsockopt()
+    * 
+    * socket의 옵션을 가져오는 함수
+    * -> ::getsockopt()
+    * 
+    * ::setsockopt() 함수의 매개 변수
+    * - int level
+    *   -> 옵션을 해석하고 처리할 주체(socket, TCP 프로토콜, IP 프로토콜 등)
+    *   -> socket 코드(socket 단계) : SOL_SOCKET
+    *   -> IPv4 프로토콜 단계 : IPPROTO_IP
+    *   -> TCP 프로토콜 단계 : IPPROTO_TCP
+    * 
+    * - int optname
+    *   -> 옵션 지정
+    *   -> SOL_SOCKET 단계의 optname 중 자주 사용되는 옵션
+    *       - SO_KEEPALIVE          : 주기적으로 연결 상태 확인 여부를 설정(TCP에서만 동작)
+    *           -> 상대방이 소리소문없이 연결을 끊는 경우가 있기 때문에 사용
+    *           -> 활성화되면 주기적으로 TCP 프로토콜의 연결 상태를 확인하는 패킷을 보내면서, 끊어진 연결을 감지 가능
+    *           {
+    *               bool enable = true;
+    *               ::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&enable, sizeof(enable));
+    *           }
+    *       - SO_LINGER             : closesocket() 함수가 동작할 때, kernel 영역에 있는 socket의 send buffer에 들어 있는 바로 제거할 지 아니면 일정 시간 동안 기다린 후에 제거할 지 설정
+    *           -> LINGER 구조체를 이용
+    *               -> LINGER 구조체의 u_short l_onoff  : 0이면 closesocket() 함수가 바로 리턴, 아니면 linger초 만큼 대기(default는 0)
+    *               -> LINGER 구조체의 u_short l_linger : 대기 시간(초 단위, second 단위)
+    *           -> 
+    *           {
+    *               LINGER linger;
+    *               linger.l_onoff = 1;
+    *               linger.l_linger = 5;
+    *               ::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
+    *           }
+    *       - SO_RCVBUF, SO_SNDBUF  : kernel 영역의 send buffer, 또는 kernel 영역의 recv buffer의 크기를 조정
+    *           -> 너무 큰 값으로 지정하면 내부적으로 무시할 수 있다.
+    *       - SO_REUSEADDR          : IP 주소 및 port를 재사용하는 옵션
+    *           -> 개발 단계에서 편하기 때문에 설정하는 것을 추천(서버가 종료되고 다시 켤 때 해당 port가 아직 바인딩이 안풀릴 수도 있기 때문)
+    * 
+    *   -> IPPROTO_TCP 단계의 optname 중 중요한 옵션
+    *       - TCP_NODELAY           : Nagle 알고리즘(네이글 알고리즘) 작동 여부 설정
+    *           -> true인 경우, Nagle 알고리즘이 작동하지 않는다.
+    *           -> 기본값은 0
+    * 
+    * 
+    * - const char* optval
+    *   -> 설정한 옵션이 저장된 변수의 주소
+    *   -> char*로 캐스팅해서 넣어줘야 한다.
+    * 
+    * - int optlen
+    *   -> optval에 넣어준 변수의 크기
+    */
+
+    /**
+    * Half-Close
+    * - socket을 반환하기 전에 kernel 영역의 send buffer, kernel 영역의 recv buffer 둘 중 하나를 닫는 방법
+    * - ::closesocket() 함수를 호출하기 전에 ::shutdown() 함수를 이용해 buffer 중 하나를 끊는다.(socket은 반환하지 않음)
+    *   - SD_RECEIVE : kernel 영역의 recv buffer로부터 데이터를 가져오는 것을 막는다.
+    *   - SD_SEND : kernel 영역의 send buffer로 전송하는 것을 막는다.(어플리케이션의 buffer에서 kernel 영역의 send buffer로 복사를 못하도록 함)
+    *     - send buffer에 데이터가 남아있다면, 남아있는 데이터는 목적지로 전송된다.
+    *   - SD_BOTH : SD_RECEIVE, SD_SEND 둘을 합쳐놓은 것
+    * - ::shutdown() 함수를 호출할 때 SHUT_WR 인자를 넣으면, 클라이언트에게는 EOF가 전송된다.
+    * - 필요성
+    *   - close() 함수 호출은 완전종료를 의미하는데, 완전종료라는 것은 데이터 전송은 물론이고 데이터 수신도 더 이상 불가능하다는 것을 의미함.
+    *   - 완전종료를 하게 되면, 다른 호스트가 전송하고 있는 데이터를 수신하지 못하는 경우가 발생할 수 있음.
+    *   - 이를 막기 위해 필요함
+    * 
+    * - 강사왈) MMORPG에서는 별의별 상황이 발생하고, Half-Close를 하지 않더라도 큰 일이 발생하는 경우은 거의 없었음
+    */
+
+    /**
+    * Nagle 알고리즘(네이글 알고리즘)
+    * - 원리 : 데이터가 충분히 크면 보내고, 그렇지 않다면 데이터가 충분히 쌓을 때까지 대기한다.
+    *   -> 최대한 데이터를 뭉쳐 보내서, 회선 낭비를 줄이겠다!
+    * - 장점 : 작은 패킷이 불필요하게 많이 생성되는 일을 방지
+    * - 단점 : 반응 시간을 손해본다.
+    * -> 게임에서는 Nagle 알고리즘을 끈다.
+    */
+
+    SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
     if (INVALID_SOCKET == clientSocket)
     {
         ErrorHandling("socket()");
         return 0;
-    }
-
-    SOCKADDR_IN serverAddr;
-    ::memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    ::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-    serverAddr.sin_port = htons(7777);
-
-    // Connected UDP //
-    // - socket에 보낼 대상의 정보(SOCKADDR_IN 구조체의 내용)을 등록하는 방식
-    // - connect() 함수를 호출해 socket에 SOCKADDR_IN 구조체의 내용을 등록한다.
-    // -> 이후 데이터를 보낼때는 send() 함수, 데이터를 수신할 때는 recv() 함수를 이용한다.
-    if (SOCKET_ERROR == ::connect(clientSocket, reinterpret_cast<SOCKADDR*>(&serverAddr), sizeof(serverAddr)))
-    {
-        ErrorHandling("connect()");
-        return 0;
-    }
-
-    // Unconnected UDP(기본적인 UDP), Connected UDP의 차이 //
-    // -> Unconnected UDP(기본적인 UDP)  : 보낼 주소의 정보(SOCKADDR_IN 구조체의 내용)을 전송할 떄마다 입력한다.
-    // -> Connected UDP                 : 보낼 주소의 정보(SOCKADDR_IN 구조체의 내용)을 전송할 떄마다 입력하지 않는다.
-
-    // UDP의 보안 측면 위험성 //
-    // - 서버에서 UDP 소켓을 열게 되면, 어느 누구라도 서버의 UDP 소켓에 데이터를 전송할 수 있기 때문에
-    //   보안적인 측면에서는 위험하다.
-    // -> TCP의 경우, 소켓끼리 1:1 연결이기 때문에 UDP보다 안전하기 때문에 가급적이면 TCP를 사용한다.
-
-    
-    while (true)
-    {
-        char sendBuffer[100] = "Hello World!";
-
-        // 나의 IP 주소, 포트 번호가 자동으로 설정됨.
-        int32 errorCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
-        if (SOCKET_ERROR == errorCode)
-        {
-            ErrorHandling("sendto()");
-            return 0;
-        }
-        std::cout << "Send Data! Len : " << sizeof(sendBuffer) << std::endl;
-
-        char recvBuffer[1000] = { NULL, };
-        int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-        if (recvLen <= 0)
-        {
-            ErrorHandling("recvfrom()");
-            return 0;
-        }
-        std::cout << "Receive Data! Data : " << recvBuffer << ", Len : " << recvLen << std::endl;
-
-        using std::chrono::operator""s;
-        std::this_thread::sleep_for(1s);
     }
 
     ::closesocket(clientSocket);
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -30,64 +30,17 @@ int main()
         return errorCode;
     }
 
-    SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
+    SOCKET serverSocket = ::socket(AF_INET, SOCK_STREAM, 0);
     if (INVALID_SOCKET == serverSocket)
     {
         ErrorHandling("socket()");
         return 0;
     }
 
-    SOCKADDR_IN serverAddr;
-    ::memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
-    serverAddr.sin_port = htons(7777);
-
-    if (SOCKET_ERROR == ::bind(serverSocket, reinterpret_cast<SOCKADDR*>(&serverAddr), sizeof(serverAddr)))
-    {
-        ErrorHandling("bind()");
-        return 0;
-    }
-
-    /**
-    * TCP 소켓과 UDP 소켓의 차이
-    * -> UDP의 경우 서버에서는 listen(), accept() 함수를 호출하지 않는다.
-    *    -> bind()까지 되었다면, 바로 recvfrom(), sendto() 함수를 호출
-    * -> Unconnected UDP(기본적인 UDP)의 경우 클라이언트에서는 connect() 함수를 호출하지 않는다.
-    *    -> 서버 정보까지 입력되었다면, 바로 recvfrom(), sendto() 함수를 호출
-    * -> UDP의 경우, 수신하는 호스트에는 server socket 하나만 있으면 된다.
-    *    -> but TCP는 수신하는 호스트에서 송신하는 호스트의 소켓을 하나 더 만든다.
-    *    -> ex) TCP의 경우, 클라이언트가 10개라면 서버에서는 11개가 생성된다.(서버 소켓 1개 + 클라이언트 소켓 10개)
-    * 
-    * client의 경우, TCP 소켓, UDP 소켓 모두 IP 주소와 port 번호를 연동하지 않는다.
-    * -> 데이터를 처음 보낼 때, 자동으로 client의 IP 주소와 port 번호가 연동된다.
-    * -> Unconnected UDP(기본적인 UDP)의 경우, sendto() 함수를 호출할 때 자동으로 client의 IP 주소와 port 번호가 연동된다.
-    * -> TCP의 경우, connect() 함수를 호출할 때 자동으로 client의 IP 주소와 port 번호가 연동된다.
-    */
-    
-    while (true)
-    {
-        char recvBuffer[1000] = { NULL, };
-        SOCKADDR_IN clientAddr = {};
-        ::memset(&clientAddr, 0, sizeof(clientAddr));
-        int32 addrLen = sizeof(clientAddr);
-
-        int32 recvLen = ::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0, reinterpret_cast<SOCKADDR*>(&clientAddr), &addrLen);
-        if (recvLen <= 0)
-        {
-            ErrorHandling("recvfrom()");
-            return 0;
-        }
-        std::cout << "Receive Data! Data : " << recvBuffer << ", Len : " << recvLen << std::endl;
-
-        int32 errorCode = ::sendto(serverSocket, recvBuffer, recvLen, 0, reinterpret_cast<SOCKADDR*>(&clientAddr), sizeof(clientAddr));
-        if (SOCKET_ERROR == errorCode)
-        {
-            ErrorHandling("sendto()");
-            return 0;
-        }
-        std::cout << "Send Data! Len : " << recvLen << std::endl;
-    }
+    LINGER linger;
+    linger.l_onoff = 1;
+    linger.l_linger = 5;
+    ::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
 
     ::closesocket(serverSocket);
     ::WSACleanup();
