5a1ca0874da4ba3f47bf80c654c4abc40457bc62
jang-taehyun <jth0202@naver.com>
2026-01-30 16:24:39 +0900

    4-45) 소켓 입출력 모델 - select 모델

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -24,16 +24,56 @@ int main()
     }
 
     /**
-    * 블로킹(Blocking) 소켓
-    * - 완료가 될때까지 대기한다.
-    *   -> TCP 소켓 기준, connect(), accept(), send(), sendto(), recv(), recvfrom() 함수 등
-    *       -> accept() 함수 : 접속한 클라가 있을 때
-    *       -> connect() 함수 : 서버 접속이 성공할 때
-    *       -> send(), sendto() 함수 : 요청한 데이터를 send buffer에 일부라도 복사했을 때
-    *       -> recv(), recvfrom() 함수 : recv buffer에 도착한 데이터가 있고, 이를 유저레벨 buffer에 복사했을 때
-    * - 완료가 될때까지 대기하기 때문에, 동시 접속자 수가 많은 경우 성능 저하가 발생
-    *   -> 그렇다고 접속자 수만큼 쓰레드를 생성하게 되면, context switching이 빈번하게 일어나기 때문에 성능 저하가 발생
+    * socket 입출력 모델
+    * - 논블로킹 소켓을 사용하면서, 준비되었을 때 미리 파악할 수 있으면 어떨까?
+    * - socket 입출력 모델을 알아야하는 이유
+    *   -> 교양적인 이유도 있고, boost::asio를 더 많이 이해하기 위해서도 필요함
     * 
+    * 1) select 모델
+    * - 핵심 : 소켓 함수 호출이 성공할 시점을 미리 알 수 있다!
+    *   - read, write를 하기 전에 read, write를 할 수 있는지 먼저 체크한다!
+    *   -> 문제 상황
+            - recv buffer에 데이터가 없는 상황에서 read를 하거나, send buffer가 가득 찬 상황에서 write를 하는 경우
+                -> 블로킹 소켓은 recv buffer에 데이터가 들어오거나, send buffer에 데이터가 들어갈 공간이 생길때까지 블로킹을 함.
+                -> 논블로킹 소켓의 경우, 리턴은 하지만 WSAEWOULDBLOCK 에러가 발생하면 다시 시도를 해야 되는 경우가 있음
+    * - select() 함수가 핵심이 되는 모델
+    * - windows, linux 모두 존재
+    * - 블로킹 소켓, 논블로킹 소켓 모두 사용 가능
+    *   -> 블로킹 소켓에 적용하는 경우 : 조건이 만족되지 않아서 블로킹되는 상황을 예방할 수 있음
+    *   -> 논블로킹 소켓에 적요하는 경우 : 조건이 만족되지 않아서 불필요하게 반복 체크하는 상황을 예방할 수 있음
+    * 
+    * - 사용법
+    *   1) socket set을 만든다.
+        2) socket set을 초기화하고 소켓을 socket set에 등록해 관찰 대상으로 만든다.
+    *       - 관찰은 읽기[ ], 쓰기[ ], 예외(OOB)[ ]로 나뉜다.
+    *           - 읽기 : read의 성공 시점을 체크(관찰)
+    *           - 쓰기 : write의 성공 시점을 체크(관찰)
+    *           - 예외 : OOB(OutOfBand) 체크
+    *               - OOB(OutOfBand) : send() 함수의 마지막 인자에 MSG_OOB로 세팅해 보내는 특별한 데이터
+    *                   - 받는 쪽에서도 recv OOB 세팅을 해야 읽을 수 있다.
+    *                   - 긴급 상황 또는 특이한 상황을 알리는 용도로 사용
+    *       - 관찰은 하나만 적용할 수 있는게 아니라, 여러 개를 적용할 수 있다.
+    *           - ex) 한 소켓의 읽기, 쓰기를 모두를 관찰하고 싶다면, 읽기, 쓰기 모두 등록 가능
+    *   3) select() 함수를 호출한다.
+    *       - select() 함수를 호출할 때, 매개변수로 recv socket set, send socket set, exception socket set을 넣어준다.
+    *           - 만약 쓰지 않는 socket set이 있다면, NULL로 넣어주면 된다.
+    *       - select() 함수를 호출하면 등록된 소켓을 대상으로 관찰을 시작한다.
+    *       - socket set에 등록된 소켓 중 준비가 완료된 소켓이 하나라도 있다면 리턴한다.
+    *           -> ex) 한 소켓은 recv socket set에, 다른 하나의 소켓은 send socket set에 등록했다면,
+    *                  둘 중 하나라도 준비가 완료되었다면 select() 함수는 리턴한다.
+    *           - select() 함수가 리턴되었을 때, 준비가 되지 않는 소켓은 관찰 대상에서 제거된다.
+    *           - select() 함수의 리턴 값 : 준비된 소켓의 개수
+    *   4) 관찰 대상에 남아있는 소켓을 대상으로 read 또는 write를 진행한다.
+    *   4) 2~4 과정을 반복한다.
+    * 
+    * - 장점
+    *   - 블로킹 소켓 : read, write 관련 함수가 리턴할 때까지 기다리지 않아도 된다.
+    *   - 논블로킹 소켓 : 반복 체크하는 상황을 예방할 수 있다.
+    *     -> 논블로킹 소켓의 경우, read/write가 가능한지 먼저 체크하기 때문에 안전하게 read/write를 진행 가능
+    * 
+    * - 단점
+    *   - 한계 : socket set에 등록할 수 있는 소켓의 수가 정해져 있음
+    *       -> socket set에 등록할 수 있는 소켓의 수가 생각보다 적음
     */
 
     SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
@@ -91,6 +131,7 @@ int main()
                 continue;
 
             // error 발생!
+            ErrorHandling("send()");
             break;
         }
 
@@ -110,11 +151,13 @@ int main()
                     continue;
 
                 // error 발생!
+                ErrorHandling("recv()");
                 break;
             }
             // 연결이 끊긴 상황
             else if (0 == recvLen)
             {
+                std::cout << "Unconnected to Server!" << std::endl;
                 break;
             }
 
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -20,6 +20,24 @@ void ErrorHandling(const char* cause)
     std::cout << cause << " ErrorCode : " << errorCode << std::endl;
 }
 
+const int32 BUFSIZE = 1000;
+
+// 접속한 클라이언트 //
+// 접속한 클라이언트만큼 Session 구조체가 생성된다.
+struct Session
+{
+    Session()
+    {
+        ::memset(&addr, 0, sizeof(addr));
+    }
+
+    SOCKET socket = INVALID_SOCKET;
+    SOCKADDR_IN addr = { 0, };
+    char recvBuffer[BUFSIZE] = { 0, };
+    int32 recvByte = 0;
+    int32 sendByte = 0;
+};
+
 int main()
 {
     int32 errorCode = 0;
@@ -64,79 +82,109 @@ int main()
         return 0;
     }
 
-    /**
-    * 논블로킹 소켓에서 WSAEWOULDBLOCK 코드가 반환되는 경우
-    * -> recv(), send()가 완료되지 않았을 때
-    * -> accept() 호출 시, 접속한 클라이언트가 없을 때
-    */
+    // session 배열 생성
+    std::vector<Session> sessions;
+    sessions.reserve(100);
 
-    std::cout << "Accept" << std::endl;
+    // socket set 생성 //
+    fd_set readSet;
+    fd_set writeSet;
+    
     while (true)
     {
-        SOCKADDR_IN clientAddr = {};
-        ::memset(&clientAddr, 0, sizeof(clientAddr));
-        int32 clientAddrSize = sizeof(clientAddr);
-        SOCKET clientSocket = ::accept(serverSocket, (SOCKADDR*)&clientAddr, &clientAddrSize);
+        // socket set 초기화 //
+        FD_ZERO(&readSet);
+        FD_ZERO(&writeSet);
 
-        // non-blocking 소켓인 경우, accept() 호출 시 접속한 클라이언트가 없다면 INVALID_SOCKET 반환 //
-        if (INVALID_SOCKET == clientSocket)
+        // 소켓을 socket set에 등록 //
+        // 서버 소켓은 read set에 등록해야 한다.
+        // -> accept할 대상이 있을 때까지 기다리기 때문
+        FD_SET(serverSocket, &readSet);
+
+        for (Session& s : sessions)
         {
-            // 접속한 클라이언트가 없는 경우(문제 상황이 아님)
-            if (WSAEWOULDBLOCK == ::WSAGetLastError())
-                continue;
+            if(s.recvByte <= s.sendByte)
+                FD_SET(s.socket, &readSet);
+            else
+                FD_SET(s.socket, &writeSet);
+        }
 
-            // error 발생!
+        // select() 함수 호출 //
+        // select() 함수의 마지막 인자
+        // -> 옵션(넣어도 되고, nullptr로 넣어도 됨)
+        // -> timeout(대기 시간)를 설정하는 부분
+        // -> nullptr이라면 소켓이 하나라도 준비될 때까지 무한 대기
+        // -> timeval 구조체의 포인터를 넣어준다면 대기 시간만큼 대기하고 리턴
+        //      -> 대기 시간만큼 기다리는 동안 소켓이 하나라도 준비되었다면 리턴
+        //      -> 대기 시간만큼 기다린 후에도 소켓이 하나라도 준비되지 않았다면 리턴
+        int32 retVal = ::select(0, &readSet, &writeSet, nullptr, nullptr);
+        if (SOCKET_ERROR == retVal)
+        {
+            ErrorHandling("select()");
             break;
         }
 
-        std::cout << "Client Connected!" << std::endl;
-
-        // recv
-        while (true)
+        // 소켓이 socket set에 등록되어 있는 지 확인 //
+        // socket set에 등록되어 있지 않으면 0을 리턴
+        // socket set에 등록되어 있으면 0이 아닌 값을 리턴
+        if (FD_ISSET(serverSocket, &readSet))
         {
-            char recvBuffer[1000] = { NULL, };
+            Session client;
+            int32 addrLen = sizeof(client.addr);
 
-            // non-blocking 소켓인 경우, recv(), recvfrom() 호출 시 recv buffer에 데이터가 없다면 SOCKET_ERROR 반환 //
-            int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-            if (SOCKET_ERROR == recvLen)
+            client.socket = ::accept(serverSocket, (SOCKADDR*)&(client.addr), &addrLen);
+            if (INVALID_SOCKET == client.socket)
             {
-                // 해당 socket의 recv buffer에 데이터가 없는 경우
-                if (WSAEWOULDBLOCK == ::WSAGetLastError())
-                    continue;
-
-                // error 발생!
-                break;
-            }
-            // 연결이 끊긴 상황
-            else if (0 == recvLen)
-            {
+                ErrorHandling("accept()");
                 break;
             }
 
-            std::cout << "Recv Data! Len : " << recvLen << std::endl;
+            std::cout << "Client Connected!" << std::endl;
 
-            // send
-            while (true)
+            sessions.push_back(client);
+        }
+
+        for (Session& s : sessions)
+        {
+            if (FD_ISSET(s.socket, &readSet))
             {
-                // non-blocking 소켓인 경우, send(), sendto() 호출 시 send buffer에 데이터가 들어갈 공간이 없다면 SOCKET_ERROR 반환 //
-                int32 sendLen = ::send(clientSocket, recvBuffer, recvLen, 0);
-                if (SOCKET_ERROR == sendLen)
+                int32 recvLen = ::recv(s.socket, s.recvBuffer, sizeof(s.recvBuffer), 0);
+                if (0 >= recvLen)
                 {
-                    // 해당 socket의 send buffer에 데이터가 없는 경우
-                    if (WSAEWOULDBLOCK == ::WSAGetLastError())
-                        continue;
+                    // TODO: sessions 제거
 
-                    // error 발생!
-                    break;
+                    continue;
                 }
 
-                std::cout << "Send Data! Len : " << sendLen << std::endl;
+                s.recvByte = recvLen;
+            }
+
+            if (FD_ISSET(s.socket, &writeSet))
+            {
+                // send() 함수 동작
+                // 블로킹 소켓 : 모든 데이터를 다 보냄
+                // 논블로킹 소켓 : 상대방 recv buffer의 상황에 따라서 일부만 보낼 수 있음
+                int32 sendLen = ::send(s.socket, &(s.recvBuffer[s.sendByte]), s.recvByte - s.sendByte, 0);
+                if (SOCKET_ERROR == sendLen)
+                {
+                    // TODO: sessions 제거
+
+                    continue;
+                }
+
+                s.sendByte += sendLen;
+                if (s.sendByte == s.recvByte)
+                {
+                    s.recvByte = 0;
+                    s.sendByte = 0;
+                }
             }
         }
     }
-    
 
-    
+    // FD_CLR() 매크로 함수
+    // - 소켓을 socket set에서 제거하는 함수
+    // - ex) FD_CLR(serverSocket, &readSet);
 
     ::closesocket(serverSocket);
     ::WSACleanup();
